726d82209a06133932747720fbddf4b523951c15
technocake <technocake@komsys.org>
2013-03-25 00:48:40 +1100

    make-switches

--- nms-simulator/make-switches.py
+++ nms-simulator/make-switches.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+#
+#	./make-switches.py > switchlist.txt
 off = [13,37,1,3]
 switches =[]
 
 def sw(i, num):
 	return "e%u-%u %s" % (i*2-1, num+1, 'off' if i in off else 'on')
 
-for i in range(42 + 1):
-	if not ( i >= 1 and i <= 5 ) :
+for i in range(1, 42 + 1):
+	if not ( i >= 1 and i <= 5 ):
 		switches.append(sw(i, 0))
 		switches.append(sw(i, 1))
 	
